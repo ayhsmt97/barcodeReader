ebbad73e9f447ec438a16d9c86f561ddda170220
ayhsmt97 <ayhsmt97@akane.wasaeda.jp>
2020-09-18 04:21:09 +0900

    change js file

--- test.js
+++ test.js
@@ -1,50 +1,3 @@
-var isbn_code = document.getElementById("isbn_code");
-
-Quagga.onDetected(function (data) {
-    isbn_code.innerHTML = data.codeResult.code;
-    console.log(data.codeResult.code);
-});
-
-Quagga.onProcessed(function(data){
-    var drawingCtx = Quagga.canvas.ctx.overlay,
-        drawingCanvas = Quagga.canvas.dom.overlay;
-    if(data){
-        if(data.boxes){
-            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-            data.boxes.filter(function (box) {
-                return box !== data.box;
-            }).forEach(function (box) {
-                Quagga.ImageDebug.drawPath(box, {
-                    x: 0,
-                    y: 1
-                }, drawingCtx, {
-                    color: "green",
-                    lineWidth: 2
-                });
-            });
-        }
-
-        if (data.box) {
-            Quagga.ImageDebug.drawPath(data.box, {
-                x: 0,
-                y: 1
-            }, drawingCtx, {
-                color: "#00F",
-                lineWidth: 2
-            });
-        }
-
-        if (data.codeResult && data.codeResult.code) {
-            Quagga.ImageDebug.drawPath(data.line, {
-                x: 'x',
-                y: 'y'
-            }, drawingCtx, {
-                color: 'red',
-                lineWidth: 3
-            });
-        }
-    }
-});
 
 function startScanner(){
     Quagga.init({
@@ -85,13 +38,13 @@ function startScanner(){
       });
 
     Quagga.onDetected(function (data) {
-        isbn_code.innerHTML = data.codeResult.code;
+        document.getElementById("isbn_code").innerHTML = data.codeResult.code;
         console.log(data.codeResult.code);
     });
 
     Quagga.onProcessed(function(data){
         if(data){
-            console.log("I can see somethig :)");
+            console.log("I got some data");
             if(data.codeResult)
                 console.log(data.codeResult.code);
         }
